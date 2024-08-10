82801dfd88f0f8cc34435c991fa35da316163a54
Dimitri Matheus <dimitrimatheusdeoliveira@gmail.com>
2024-08-09 20:33:02 -0400

    New layout for section option
    Add new buttons and formating option.html and css

--- script.js
+++ script.js
@@ -124,44 +124,28 @@ function generateRandomNumber(min, max) {
 
 // dark-mode
 document.addEventListener('DOMContentLoaded', function () {
-    const themes = {
-        light: 'claro',
-        dark: 'escuro'
-    };
-
     function initThemeLoader() {
-        const themeLoader = document.getElementById('theme')
+        const themeLoader = document.getElementById('themeToggle')
         const themeStylesheetLink = document.getElementById('themeStylesheetLink')
         let currentTheme = localStorage.getItem('theme') || 'light'
 
-        updateTheme(currentTheme)
-
-        function changeTheme () {
-            currentTheme = currentTheme === 'light' ? 'dark' : 'light'
-
-            const body = document.querySelector('body')
-            body.style.transition = 'background-color .5s, color .5s linear'
-            body.style.transition = ''
-            void body.offsetWidth
-            body.style.transition = 'background-color .5s, color .5s linear'
-            
-            themeStylesheetLink.setAttribute('href', `css/${currentTheme}.css`)
-            localStorage.setItem('theme', currentTheme)
-            updateTheme(currentTheme)
-            console.log(`O estilo atual é ${currentTheme}`)
-            console.log(body)
-        }
-
-        function updateTheme(theme) {
-            if (themeLoader) {
-                const oppositeTheme = theme === 'light' ? 'dark' : 'light'
-                themeLoader.innerHTML = `Modo ${themes[oppositeTheme]}`
-            }
-        }
-
         if (themeLoader) {
-            themeLoader.addEventListener('click', changeTheme)
+            themeLoader.checked = currentTheme === 'dark'
+
+            themeLoader.addEventListener('click', function () {
+                currentTheme = themeLoader.checked ? 'dark' : 'light'
+                
+                const body = document.querySelector('body')
+                body.style.transition = 'background-color .5s, color .5s linear'
+                body.style.transition = ''
+                void body.offsetWidth
+                body.style.transition = 'background-color .5s, color .5s linear'
+
+                themeStylesheetLink.setAttribute('href', `css/${currentTheme}.css`)
+                localStorage.setItem('theme', currentTheme)
+            })
         }
+        console.log(`O estilo atual é ${currentTheme}`)
         activateTheme(currentTheme)
     }
 
